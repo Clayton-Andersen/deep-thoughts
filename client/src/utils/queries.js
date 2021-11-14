07a566b0e958af5f858c2c3a88e2b015b9c02bdc
Aloha <utah.clayton@gmail.com>
2021-11-14 01:45:44 -0800

    profile page complete

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,3 +17,24 @@ query thoughts($username: String) {
     }
 }
 `;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
